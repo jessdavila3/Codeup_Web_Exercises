71e86f0fa55bcb4b687f9c0f75db02bae192c067
Jesse Davila <jessedavila3@gmail.com>
2016-10-18 15:24:49 -0500

    fixed grid positioning. still have very basic functionality on the actual calculator stuff.

--- js/calculator.js
+++ js/calculator.js
@@ -30,16 +30,16 @@ function operate() { //this determines how or when to calculate, or to clear
 
 function calculate(operation) { // finds operator number and performs operation on left and right variables.
     if (operation == 1) {
-        right.innerHTML = parseInt(left.innerHTML) + parseInt(right.innerHTML);
+        right.innerHTML = parseFloat(left.innerHTML) + parseFloat(right.innerHTML);
         left.innerHTML = '';
     } else if (operation == 2) {
-        right.innerHTML = parseInt(left.innerHTML) - parseInt(right.innerHTML);
+        right.innerHTML = parseFloat(left.innerHTML) - parseFloat(right.innerHTML);
         left.innerHTML = '';
     } else if (operation == 3) {
-        right.innerHTML = parseInt(left.innerHTML) * parseInt(right.innerHTML);
+        right.innerHTML = parseFloat(left.innerHTML) * parseFloat(right.innerHTML);
         left.innerHTML = '';
     } else if (operation == 4) {
-        right.innerHTML = parseInt(left.innerHTML) / parseInt(right.innerHTML);
+        right.innerHTML = parseFloat(left.innerHTML) / parseFloat(right.innerHTML);
         left.innerHTML = '';
     } else {
     alert('error');
@@ -50,21 +50,30 @@ function calculate(operation) { // finds operator number and performs operation 
 
 function populate() {
     var selected = this;
-    if (refresh) { /*this is a on off switch that resets the display after pressing a new number once you've hit equals*/
+    if (refresh && isNaN(selected.innerHTML)) { /*this is a on off switch that resets the display after pressing a new number once you've hit equals*/
+        left.innerHTML = right.innerHTML;
+        right.innerHTML = '';
+        refresh = false;
+    } else if (refresh) {
         left.innerHTML = '';
         right.innerHTML = '';
         operand.innerHTML = '';
         refresh = false;
     }
-    if (!isNaN(selected.innerHTML)) {
+    if (!isNaN(selected.innerHTML)) { /*if it's a number, place it appropriately*/
         if (operand.innerHTML == '') {
             left.innerHTML += selected.innerHTML;
         } else if (operand.innerHTML !== '') {
             right.innerHTML += selected.innerHTML;
         }
-    } else if (isNaN(selected.innerHTML)) {
-        operand.innerHTML = selected.innerHTML;
-        operate(selected.innerHTML);
+    } else if (isNaN(selected.innerHTML)) { /*if it's not a number, add it to the operand section and perform the operate function*/
+        if (left.innerHTML !== '') {
+            operand.innerHTML = selected.innerHTML;
+            operate(selected.innerHTML);
+        } else if (left.innerHTML == '') {
+            alert('please select a number to perform an action on first')
+        }
+
     }
 }
 
