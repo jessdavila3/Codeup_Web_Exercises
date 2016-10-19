45d1d498cf830ba7c1f3b9f8f64d43955b2a87d9
Jesse Davila <jessedavila3@gmail.com>
2016-10-19 12:22:49 -0500

    added a history section.

--- js/calculator.js
+++ js/calculator.js
@@ -5,16 +5,34 @@
 var left = document.getElementById('leftNumberDisplay');
 var operand = document.getElementById('operandDisplay');
 var right = document.getElementById('rightNumberDisplay');
+var historytext = document.getElementById('text');
 var operation = 0;
 var refresh = false;
+var buttons = document.getElementsByClassName('btn');
 
-function clearDisplay(operation) {
-    if (operation) {
-        left.innerHTML = '';
-    } else {
-        left.innerHTML = '';
-        right.innerHTML = '';
-        operand.innerHTML = '';
+for (var i = 0; buttons.length > i; i++) {
+    buttons[i].addEventListener('click', populate, false);
+
+}
+
+function populate() {
+    var selected = this;
+    refreshDisplay(refresh, selected);
+    toNegative(selected);
+    if (!isNaN(selected.innerHTML) || selected.innerHTML == '.') { /*if it's a number or a point, place it appropriately*/
+        if (operand.innerHTML == '') {
+            left.innerHTML += selected.innerHTML;
+        } else if (operand.innerHTML !== '') {
+            right.innerHTML += selected.innerHTML;
+        }
+    } else if (isNaN(selected.innerHTML) && selected.innerHTML !== '+/-') { /*if it's not a number, add it to the operand section and perform the operate function*/
+        if (left.innerHTML !== '') {
+            operand.innerHTML = selected.innerHTML;
+            operate(selected.innerHTML);
+        } else if (left.innerHTML == '') {
+            alert('please select a number to perform an action on first')
+            clearDisplay(selected);
+        }
     }
 }
 
@@ -37,6 +55,9 @@ function operate() { //this determines how or when to calculate, or to clear
 }
 
 function calculate(operation) { // finds operator number and performs operation on left and right variables.
+    var i = 0;
+    var historyOperand = operation;
+    logHistory(historyOperand);
     if (operation == 1) {
         right.innerHTML = parseFloat(left.innerHTML) + parseFloat(right.innerHTML);
         clearDisplay(operation);
@@ -52,7 +73,18 @@ function calculate(operation) { // finds operator number and performs operation 
     } else {
         alert('error');
     }
+    logHistory();
     refresh = true;
+}
+
+function clearDisplay(operation) {
+    if (operation) {
+        left.innerHTML = '';
+    } else {
+        left.innerHTML = '';
+        right.innerHTML = '';
+        operand.innerHTML = '';
+    }
 }
 
 function refreshDisplay(resetDisplay, number) {
@@ -65,27 +97,38 @@ function refreshDisplay(resetDisplay, number) {
     refresh = false;
 }
 
-function populate() {
-    var selected = this;
-    refreshDisplay(refresh, selected);
-    if (!isNaN(selected.innerHTML) || selected.innerHTML == '.') { /*if it's a number or a point, place it appropriately*/
-        if (operand.innerHTML == '') {
-            left.innerHTML += selected.innerHTML;
-        } else if (operand.innerHTML !== '') {
-            right.innerHTML += selected.innerHTML;
+function toNegative(specialKey) {
+    if (specialKey.innerText == '+/-') {
+        if (right.innerHTML == '') {
+            left.innerHTML *= -1;
+        } else if (right.innerHTML !== '') {
+            right.innerHTML *= -1;
         }
-    } else if (isNaN(selected.innerHTML)) { /*if it's not a number, add it to the operand section and perform the operate function*/
-        if (left.innerHTML !== '') {
-            operand.innerHTML = selected.innerHTML;
-            operate(selected.innerHTML);
-        } else if (left.innerHTML == '') {
-            alert('please select a number to perform an action on first')
-        }
-
     }
 }
 
-var buttons = document.getElementsByClassName('btn');
-for (var i = 0; buttons.length > i; i++) {
-    buttons[i].addEventListener('click', populate, false);
+function logHistory(operand) {
+    if (i % 2 == 0) {
+        if (operand == 1) {
+            historytext.innerHTML += left.innerHTML + " + " + right.innerHTML;
+        }
+        if (operand == 2) {
+            historytext.innerHTML += left.innerHTML + " - " + right.innerHTML;
+        }
+        if (operand == 3) {
+            historytext.innerHTML += left.innerHTML + " * " + right.innerHTML;
+        }
+        if (operand == 4) {
+            historytext.innerHTML += left.innerHTML + " / " + right.innerHTML;
+        }
+    } else {
+        historytext.innerHTML += ' = ' + right.innerHTML + '<br>';
+    }
+    i++;
 }
+
+
+
+
+
+
